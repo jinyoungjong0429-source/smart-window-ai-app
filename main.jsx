3cc699c60d28447a2fd0dc49bbfdcaccca54803c
jinyoungjong0429-source <jinyoungjong0429@gmail.com>
2026-05-06 15:00:16 +0900

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom/client";
+import SmartWindowAIApp from "./SmartWindowAIApp.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SmartWindowAIApp />);
